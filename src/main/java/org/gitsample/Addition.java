fcbe581fed8dde1794898cf4d6e052113f5e75b3
kravikumark <kravikumar.1104@gmail.com>
2025-04-19 15:48:21 +0530

    Addtion of 2 number

---
+++ src/main/java/org/gitsample/Addition.java
@@ -0,0 +1,16 @@
+package org.gitsample;
+
+public class Addition {
+	
+	
+	public static void main(String[] args) {
+		int a=10,b=20,c;
+		
+	c =a+b;
+	System.out.println("Addition of a+b ="+ c);
+	
+	}
+	
+	
+
+}
